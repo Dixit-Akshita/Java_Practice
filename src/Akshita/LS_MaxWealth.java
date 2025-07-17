009adc17a8ddcfa9c87f7fd5b35949e444b7f58a
Akshita Dixit <akshita15dec@gmail.com>
2025-07-17 19:57:33 +0530

    Add files via upload

---
+++ src/Akshita/LS_MaxWealth.java
@@ -0,0 +1,28 @@
+package Akshita;
+
+public class LS_MaxWealth {
+    public static void main(String[] args) {
+    }
+    public int maximumWealth(int[][] accounts) {
+        // person = row
+        // account = col
+        int ans = Integer.MIN_VALUE;
+        for (int person = 0; person < accounts.length; person++) {
+            // when you start a new row, take a new sum for that row
+            int sum = 0;
+            for (int account = 0; account < accounts[person].length ; account++) {
+                sum += accounts[person][account];
+            }
+            // now we have sum of accounts of person
+            // check with overall ans
+            if (sum > ans) {
+                ans = sum;
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
